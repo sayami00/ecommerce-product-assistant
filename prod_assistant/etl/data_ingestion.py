5d53c97c6bbb7a0a1be21a226b05eceab96aa673
Ramesh Manandhar <sayami00@gmail.com>
2025-09-29 22:08:35 +0545

    updated skeleton

--- prod_assistant/etl/data_ingestion.py
+++ prod_assistant/etl/data_ingestion.py
@@ -13,23 +13,44 @@ class DataIngestion:
     """
     
     def __init__(self):
+        """
+        Initialize environment variables, embedding model, and set CSV file path.
+        """
         pass
 
     def _load_env_variables(self):
+        """
+        Load and validate required environment variables.
+        """
         pass
 
     def _get_csv_path(self):
+        """
+        Get path to the CSV file located inside 'data' folder.
+        """
         pass
 
     def _load_csv(self):
+        """
+        Load product data from CSV.
+        """
         pass
 
     def transform_data(self):
+        """
+        Transform product data into list of LangChain Document objects.
+        """
         pass
 
     def store_in_vector_db(self):
+        """
+        Store documents into AstraDB vector store.
+        """
         pass
 
     def run_pipeline(self):
+        """
+        Run the full data ingestion pipeline: transform data and store into vector DB.
+        """
         pass
     
